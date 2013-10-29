3e544046c192e2b7be2b18bd39fc2d3f84453239
JHNU12 <pel0n3@hotmail.com>
2013-10-29 09:22:42 -0600

    directorio

---
+++ directorio.php
@@ -0,0 +1,12 @@
+<?php
+$directorio=array(
+                  array("nombre"=>"juan">
+                         "tel"=>"4477123"
+                        )
+                  array("nombre"=>"ray">
+                         "tel"=>"4718456"
+                        )
+);
+
+echo "El telefono de Ray es ".$datos[1]["tel"];
+?>
